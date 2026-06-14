309806e42abcaf61e54a2408c043cb64ad8928bc
Manindra <manindragupta009@gmail.com>
2026-06-15 00:36:24 +0530

    modified:   src/pages/Projects.jsx added onClick handler to navigate to create project page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -182,6 +182,7 @@ function ProjectColumn({ title, count, projects, type, isAdminPanelOpen, isUserP
         </div>
         {isAdminPanelOpen && !isUserPanelOpen && (
           <button
+            onClick={() => navigate('/create-project')}
             className="flex h-7 w-7 items-center justify-center rounded-full border bg-white text-text-medium transition-all hover:bg-bg-hover cursor-pointer"
             style={{
               borderColor: colors.borderDefault,
