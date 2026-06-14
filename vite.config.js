7cd721cdbdcdd322d197b80dd9f2a7fe7b556cad
Manindra <manindragupta009@gmail.com>
2026-06-14 16:21:39 +0530

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,19 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: [
+      'hr-system-production-91b2.up.railway.app'
+    ]
+  }
 })
